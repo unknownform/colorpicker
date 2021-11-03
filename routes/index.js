3aeb362e645e619bea00fa4a3d9ccee717f089d6
3ue4 <3ue4@fresnou.org>
2021-11-03 12:54:04 -0700

    initial commit project

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index',{
+    color: "#FFFFFF",
+    textColor: "#000000"
+ });
+});
+
+/*post user choices and re-render the page*/
+router.post('/', function(req, res){
+  res.render('index',{
+    color: req.body.colorPicker,
+    textColor: req.body.textColorPicker
+  })
+});
+
+/*Post random values and render original page*/
+router.post( '/random', function(req, res){
+  res.render( 'index', {
+    color: generateRandomHexCode(),
+    textColor: generateRandomHexCode()
+  })
+});
+function generateRandomHexCode(){
+  let hexCode = "#"
+  while ( hexCode.length < 7 ) {
+    hexCode +=(Math.round( Math.random()* 15)).toString( 16)
+  }
+  return hexCode
+}
